6d95ef6fa705a8c50879a1249f9e1b3a767beb83
mantelt <thomas.mantel@mavt.ethz.ch>
2022-06-22 15:15:42 +0200

    Adjusting jetson_env.sh
    - using hostnames works, no need to set ROS_IP
    - no need to set ROS_MASTER_URI

--- object_detection/launch/jetson_env.sh
+++ object_detection/launch/jetson_env.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export ROS_IP=192.168.55.1
-export ROS_MASTER_URI=http://192.168.55.100:11311
 export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1:/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0
 source /home/robotx/catkin_workspaces/smb_dev/devel/setup.bash
 
