1852d3acc6da85272cd56e866b57486294263412
mantelt <thomas.mantel@mavt.ethz.ch>
2022-06-24 10:19:16 +0200

    Changing jetson setup/environment
    ! Now, the jetson_env.sh needs to be in the user home directory (~) !
    Easy way to still follow changes: create a symlink as follows.
    
    ```
    roscd object_detection
    ln -s $PWD/launch/jetson_env.sh ~/jetson_env.sh
    ```

--- object_detection/launch/jetson_env.sh
+++ object_detection/launch/jetson_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1:/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0
-source /home/robotx/catkin_workspaces/smb_dev/devel/setup.bash
+source ~/object_detection_ws/devel/setup.bash
 
 exec "$@"
