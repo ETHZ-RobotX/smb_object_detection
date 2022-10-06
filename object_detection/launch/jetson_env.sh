a46e6e2ad80cb5c914cd93e1d06a661de73b534c
SMB264 <smb264@ethz.ch>
2022-10-06 14:04:44 +0200

    adjust to new catkin ws and topic names

--- object_detection/launch/jetson_env.sh
+++ object_detection/launch/jetson_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1:/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0
-source ~/object_detection_ws/devel/setup.bash
+source ~/smb_ws/devel/setup.bash
 
 exec "$@"
